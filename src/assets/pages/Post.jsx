4b5cb8ef655ad9cf3dd7256b5b9ec64533c1130a
samyusuf101 <samyusuf101@gmail.com>
2023-03-18 08:57:58 -0700

    updated post page

--- src/assets/pages/Post.jsx
+++ src/assets/pages/Post.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { preview } from "../../assets";
-
 import { Loader, Forms } from "../../components";
 
 const Post = () => {
@@ -50,6 +49,26 @@ const Post = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div
+            className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+          focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex
+          justify-center items-center"
+          >
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-400"
+              />
+            )}
+          </div>
         </div>
       </form>
     </section>
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
-const Forms = () => {
+const Forms = ({
+  LabelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <div className="flex items-center gap-2 md-2"></div>
+      <div className="flex items-center gap-2 md-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {LabelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-green-400 py-1 px-2  rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-500 m-0.5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] 
+      focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </div>
   );
 };
