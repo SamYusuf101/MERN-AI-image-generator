a2fe2f26748697ce580e4778efc0933a18868a72
samyusuf101 <samyusuf101@gmail.com>
2023-03-20 08:01:47 -0700

    updated openAi backend

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
+import aiRoutes from "./Routes/aiRoutes.js";
+import postRoutes from "./Routes/postRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/ai", aiRoutes);
+app.use("/api/v1/post", postRoutes);
 
 app.get("/", async (req, res) => {
   res.send("hello from open ai");
