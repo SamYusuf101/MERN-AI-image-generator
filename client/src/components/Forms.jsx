a92f337d563ad7e3bbad8100aebaff8dfe624c53
Sam Yusuf <samyusuf101@gmail.com>
2023-08-08 19:45:25 +0100

    Update Forms.jsx

--- client/src/components/Forms.jsx
+++ client/src/components/Forms.jsx
@@ -37,7 +37,7 @@ const Forms = ({
         value={value}
         onChange={handleChange}
         required
-        className="bg-gray-500 m-0.5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] 
+        className="bg-gray-600 m-0.5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] 
       focus:border-[#4649ff] outline-none block w-full p-3"
       />
     </div>
